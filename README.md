f8c765d773f1d4ef693a586c1a0084ac51f1b25c
Eoin Campbell <campbeeo@tcd.ie>
2021-04-12 14:25:12 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,18 @@
+# Moodle Plugins for Microsoft Word
+
+## Microsoft Word File Import/Export Local Plugin
+
+This plugin is a shell plugin that has dependencies on all Microsoft Word File Import/Export set plugins.
+These plugins allow Word files to be imported into text fields (Atto), Books, Glossaries, Lessons and the Question bank.
+They also allow Books, Glossaries, Lessons and Question bank categories to be exported into Word files for round-trip editing.
+
+This plugin requires all the plugins in the suite of 
+[Microsoft Word File Import/Export set](https://moodle.org/plugins/browse.php?list=set&id=82]) plugins for Moodle.
+
+## Copyright
+
+Code for this plugin is licensed under the GPLv3 license.
+
+Any Microsoft trademarks and logos included in these plugins are property of Microsoft and should not be reused, redistributed,
+modified, repurposed, or otherwise altered or used outside of this plugin.
+
---
+++ version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local_wordimport
+ * @author Eoin Campbell
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2021040900;
+$plugin->requires = 2020110900; // Moodle 3.10
+$plugin->release = '1.0.0';
+$plugin->component = 'local_wordimport';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = [
+    'atto_wordimport' => 2021040900,
+    'booktool_wordimport' => 2021040900,
+    'glossary_wordimport' => 2021040900,
+    'lesson_wordimport' => 2021040900,
+    'qformat_wordtable' => 2021040900,
+];
