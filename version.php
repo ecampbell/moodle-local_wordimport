9e3688e320dd3f56345db60f1505a603259a3879
Eoin Campbell <campbeeo@tcd.ie>
2021-08-31 13:09:19 +0100

    Updates for improving Lesson import plugin support

--- version.php
+++ version.php
@@ -25,15 +25,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021082300;
+$plugin->version = 2021083100;
 $plugin->requires = 2018051700; // Moodle 3.5.
-$plugin->release = '1.1.4 (Build: 2021082300)';
+$plugin->release = '1.1.5 (Build: 2021083100)';
 $plugin->component = 'local_wordimport';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'atto_wordimport' => 2021071400, // Release 1.4.1.
-    'booktool_wordimport' => 2021082100, // Release 1.4.10.
-    'local_glossary_wordimport' => 2021082100, // Release 1.1.5.
-    'local_lesson_wordimport' => 2021082100, // Release 1.0.0.
-    'qformat_wordtable' => 2021082300 // Release 3.9.1.
+    'atto_wordimport' => 2021083100, // Release 1.4.2.
+    'booktool_wordimport' => 2021083100, // Release 1.4.11.
+    'local_glossary_wordimport' => 2021083100, // Release 1.1.6.
+    'local_lesson_wordimport' => 2021083100, // Release 1.0.1.
+    'qformat_wordtable' => 2021083100 // Release 3.9.2.
 ];
