7e0741d91c3ae1b18a006447ae6c9884585e515b
Eoin Campbell <campbeeo@tcd.ie>
2025-04-24 17:13:03 +0100

    Fix code style issues, relax Moodle version requirement to 3.9

--- version.php
+++ version.php
@@ -25,15 +25,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025041500;
-$plugin->requires = 2025041400; // Moodle 5.0 or higher.
-$plugin->release = '1.2.2 (Build: 2025041500)';
+$plugin->version = 2025042400;
+$plugin->requires = 2020060900; // Moodle 3.9 or higher.
+$plugin->release = '1.2.2 (Build: 2025042400)';
 $plugin->component = 'local_wordimport';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'booktool_wordimport' => 2025041501, // Release 1.4.13.
-    'local_glossary_wordimport' => 2025041500, // Release 1.1.7.
-    'local_lesson_wordimport' => 2025041500, // Release 1.0.3.
-    'qformat_wordtable' => 2025041500, // Release 3.9.7.
+    'booktool_wordimport' => 2025042300, // Release 1.4.14.
+    'local_glossary_wordimport' => 2025042300, // Release 1.1.7.
+    'local_lesson_wordimport' => 2025042400, // Release 1.0.3.
+    'qformat_wordtable' => 2025042400, // Release 3.9.7.
     'tiny_wordimport' => 2024112000, // Release 1.1.2.
 ];
