8e8eea9d7dcb4a1cfc4832e5bb87bce2a502fa9d
Eoin Campbell <campbeeo@tcd.ie>
2025-04-25 15:43:48 +0100

    Remove Atto plugin dependency, require Moodle 5.0

--- version.php
+++ version.php
@@ -25,15 +25,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025041400;
-$plugin->requires = 2025041400; // Moodle 5.0 or higher.
-$plugin->release = '1.2.1 (Build: 2025041400)';
+$plugin->version = 2025042503;
+$plugin->requires = 2025040800; // Moodle 5.0 or higher.
+$plugin->release = '1.3.0 (Build: 2025042503)';
 $plugin->component = 'local_wordimport';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'booktool_wordimport' => 2023021700, // Release 1.4.12.
-    'local_glossary_wordimport' => 2021083100, // Release 1.1.6.
-    'local_lesson_wordimport' => 2023022400, // Release 1.0.2.
-    'qformat_wordtable' => 2025040400, // Release 3.9.6.
-    'tiny_wordimport' => 2024112000 // Release 1.1.2.
+    'booktool_wordimport' => 2025042300, // Release 1.4.14.
+    'local_glossary_wordimport' => 2025042300, // Release 1.1.7.
+    'local_lesson_wordimport' => 2023022500, // Release 1.0.4.
+    'qformat_wordtable' => 2025042300, // Release 3.9.7.
+    'tiny_wordimport' => 2024112000, // Release 1.1.2.
 ];
